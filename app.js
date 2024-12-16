7e486186584ad4a61daac312a32342741f774ccd
Harshit-7373 <harshit.sansanwal2528@gmail.com>
2024-12-16 23:56:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, "0.0.0.0", () => {
-    console.log("Server is running on http://localhost:3000");
+const port = process.env.PORT || 3000;
+
+server.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
- 
